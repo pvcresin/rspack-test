a9bf488360a63b9d443328310f9f4411ff4304f7
pvcresin <pvcresin0730@gmail.com>
2023-03-13 23:51:33 +0900

    env and analyzer

--- rspack.config.js
+++ rspack.config.js
@@ -1,41 +1,49 @@
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 /**
  * @type {import('@rspack/cli').Configuration}
  */
-module.exports = {
-  context: __dirname,
-  entry: {
-    main: "./src/main.tsx",
-  },
-  builtins: {
-    html: [{ template: "./index.html" }],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx$/,
-        use: [{ loader: "import-glob-loader" }],
-        type: "tsx",
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: ["postcss-preset-env"],
+module.exports = function (env) {
+  const analyze = !!env.analyze;
+
+  return {
+    context: __dirname,
+    entry: {
+      main: "./src/main.tsx",
+    },
+    builtins: {
+      html: [{ template: "./index.html" }],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx$/,
+          use: [{ loader: "import-glob-loader" }],
+          type: "tsx",
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: ["postcss-preset-env"],
+                },
               },
             },
-          },
-          { loader: "sass-loader" },
-          { loader: "import-glob-loader" },
-        ],
-        type: "css",
-      },
-      {
-        test: /\.png$/,
-        type: "asset",
-      },
-    ],
-  },
+            { loader: "sass-loader" },
+            { loader: "import-glob-loader" },
+          ],
+          type: "css",
+        },
+        {
+          test: /\.png$/,
+          type: "asset",
+        },
+      ],
+    },
+    plugins: analyze ? [new BundleAnalyzerPlugin()] : [],
+  };
 };
